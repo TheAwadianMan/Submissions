691cb577b39c6f01f8c7d2a7aa43818eba163f35
Omar Awad <omaar.awad.89@gmail.com>
2019-06-12 13:58:33 +0300

    step 3

--- Challenge/movie-database/Server.js
+++ Challenge/movie-database/Server.js
@@ -4,18 +4,36 @@ const port = 3000;
 
 const app = express();
 
-app.listen(port, function () {
-    console.log("Server is running on "+ port +" port");
-  });
 
 app.get('/', function(req, res) {
     console.log(req)
     console.log(res)
     res.send({message:"ok", status:"yay", id:1});
-})
+});
+
+
+app.get('/test', function(req, res) {
+    console.log(req)
+    console.log(res)
+    res.send({message:"ok", status:200, id:1});
+});
+
+app.get('/time', function(req, res) {
+    console.log(req)
+    console.log(res)
+    var time = new Date();
+     var message= time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+    res.send({message:message, status:200, id:1})
+
+});
+
 app.get('/hello/:x' , function(req, res) {
     console.log(req)
     console.log(res)
     const n= req.query.name;
     res.send({url:req.url, query:n, paramater:req.params.x});
-})
+});
+
+app.listen(port, function () {
+    console.log("Server is running on "+ port +" port");
+  });
