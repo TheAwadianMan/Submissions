8c1f39431247817b90e2d09ecfe379bf99a8e521
Omar Awad <omaar.awad.89@gmail.com>
2019-06-12 20:00:54 +0300

    step 5

--- Challenge/movie-database/Server.js
+++ Challenge/movie-database/Server.js
@@ -4,13 +4,19 @@ const port = 3000;
 
 const app = express();
 
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 
 app.get('/', function(req, res) {
     console.log(req)
     console.log(res)
     res.send({message:"ok", status:"yay", id:1});
 });
-
 
 app.get('/test', function(req, res) {
     console.log(req)
@@ -49,6 +55,42 @@ app.get("/search", function(req, res) {
   } else {
     res.send({status: 500, error: "true", message: "you have to provide a search"});
   }
+});
+
+app.get("/movies/read", function(req, res) {
+  res.send({status: 200, data: movies});
+});
+
+app.get("/movies/read/by-date", function(req, res) {
+  res.send({status: 200, data: movies.sort(function(a,b) {
+      return (b.year) - (a.year); })
+  })
+});
+
+app.get("/movies/read/by-rating", function(req, res) {
+  res.send({
+    status: 200,
+    data: movies.sort(function(a,b) {
+      return (b.rating) - (a.rating); })
+  })
+});
+
+
+
+app.get("/movies/create", function(req, res) {
+  TITLE = req.query.title
+  YEAR = req.query.year
+  RATING = req.query.rating
+ 
+  const rating= (RATING ?  RATING :  "5")
+
+  if(TITLE != undefined || YEAR != undefined || YEAR != NaN || YEAR.length != 4){
+    movies.push({TITLE,YEAR, rating})
+    res.send(movies)
+  }else{
+    res.send({status:403, error:true, message:'you cannot create a movie without providing a title and a year'})
+  }
+
 });
     
 app.get('/hello/:x' , function(req, res) {
