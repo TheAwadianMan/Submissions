0b4ace7fe23e8c032b4dbf8be0f722ee3f0804b8
Omar Awad <omaar.awad.89@gmail.com>
2019-06-12 17:52:47 +0300

    step 4

--- Challenge/movie-database/Server.js
+++ Challenge/movie-database/Server.js
@@ -27,6 +27,30 @@ app.get('/time', function(req, res) {
 
 });
 
+app.get("/hello/:Id?", function(req, res) {
+    console.log(req)
+    console.log(res)
+  var Id = req.params.Id;
+  function check() {
+    if (Id == undefined) {
+      return "";}
+     else {
+      return Id;}
+  }
+  res.send({ status:200, message:"Hello " + check() });
+
+});
+
+app.get("/search", function(req, res) {
+
+  var search = req.query.s;
+  if (search !== "") {
+    res.send({ status: 200, message: "ok", data: search});
+  } else {
+    res.send({status: 500, error: "true", message: "you have to provide a search"});
+  }
+});
+    
 app.get('/hello/:x' , function(req, res) {
     console.log(req)
     console.log(res)
